a26ee6675719673eb7d0b2f0eb8af187d20211c5
biud436 <biud436@gmail.com>
2022-03-06 18:13:01 +0900

    fixed file path

--- src/Packer.ts
+++ src/Packer.ts
@@ -1,7 +1,7 @@
 import {
-    compressScriptFiles,
-    RubyCompressScriptService,
-    RubyScriptService,
+  compressScriptFiles,
+  RubyCompressScriptService,
+  RubyScriptService,
 } from "./commands/ExtractScriptFiles";
 import { ConfigService } from "./ConfigService";
 import { LoggingService } from "./LoggingService";
@@ -9,52 +9,51 @@ import { TARGET_SCRIPT_FILE_NAME, Unpacker } from "./Unpacker";
 import * as path from "path";
 
 export class Packer extends Unpacker {
-    constructor(configService: ConfigService, loggingService: LoggingService) {
-        super(configService, loggingService);
+  constructor(configService: ConfigService, loggingService: LoggingService) {
+    super(configService, loggingService);
+  }
+
+  initWithTargetFile() {
+    const root = this.configService.getMainGameFolder().fsPath;
+    const targetFile = path
+      .join(root, "Data", TARGET_SCRIPT_FILE_NAME)
+      .replace(/\\/g, "/");
+
+    this._targetFile = targetFile;
+    this._isReady = true;
+  }
+
+  pack() {
+    if (!this._isReady) {
+      this.loggingService.info("Unpacker is not ready.");
+      throw new Error("Unpacker is not ready.");
     }
 
-    initWithTargetFile() {
-        const root = this.configService.getMainGameFolder().path;
-        const targetFile = path
-            .join(root, "Data", TARGET_SCRIPT_FILE_NAME)
-            .replace(/\\/g, "/");
+    const targetFile = <string>this._targetFile;
 
-        this._targetFile = targetFile;
-        this._isReady = true;
-    }
-
-    pack() {
-        if (!this._isReady) {
-            this.loggingService.info("Unpacker is not ready.");
-            throw new Error("Unpacker is not ready.");
+    try {
+      // Create ruby script service
+      const rubyScriptService = new RubyCompressScriptService(
+        this.configService,
+        this.loggingService,
+        {
+          vscodeWorkspaceFolder: this.configService.getVSCodeWorkSpace().fsPath,
+          scriptFile: targetFile,
+        },
+        (err: any, stdout: any, stderr: any) => {
+          if (err) {
+            this.loggingService.info(err);
+          }
+          this.loggingService.info("컴파일이 완료되었습니다");
         }
+      );
 
-        const targetFile = <string>this._targetFile;
-
-        try {
-            // Create ruby script service
-            const rubyScriptService = new RubyCompressScriptService(
-                this.configService,
-                this.loggingService,
-                {
-                    vscodeWorkspaceFolder:
-                        this.configService.getVSCodeWorkSpace().path,
-                    scriptFile: targetFile,
-                },
-                (err: any, stdout: any, stderr: any) => {
-                    if (err) {
-                        this.loggingService.info(err);
-                    }
-                    this.loggingService.info("컴파일이 완료되었습니다");
-                }
-            );
-
-            compressScriptFiles<RubyCompressScriptService>(
-                this.loggingService,
-                rubyScriptService
-            );
-        } catch (e) {
-            this.loggingService.info((<Error>e).message);
-        }
+      compressScriptFiles<RubyCompressScriptService>(
+        this.loggingService,
+        rubyScriptService
+      );
+    } catch (e) {
+      this.loggingService.info((<Error>e).message);
     }
+  }
 }
--- src/extension.ts
+++ src/extension.ts
@@ -7,72 +7,68 @@ import { Packer } from "./Packer";
 import { Unpacker } from "./Unpacker";
 
 export function activate(context: vscode.ExtensionContext) {
-    const loggingService = new LoggingService();
-    const configService: ConfigService = new ConfigService();
-    let isReady = false;
+  const loggingService = new LoggingService();
+  const configService: ConfigService = new ConfigService();
+  let isReady = false;
 
-    // 컨텍스트 설정
-    configService.setExtensionContext(context);
+  // 컨텍스트 설정
+  configService.setExtensionContext(context);
 
-    if (!vscode.workspace.workspaceFolders) {
-        loggingService.info("작업 폴더가 지정되어 있지 않습니다.");
-        throw new Error("작업 폴더가 지정되어 있지 않습니다.");
+  if (!vscode.workspace.workspaceFolders) {
+    loggingService.info("작업 폴더가 지정되어 있지 않습니다.");
+    throw new Error("작업 폴더가 지정되어 있지 않습니다.");
+  }
+
+  const workspaces = vscode.workspace.workspaceFolders;
+  configService.setVSCodeWorkSpace(workspaces[0].uri);
+
+  loggingService.info("RGSS Script Compiler가 실행되었습니다");
+
+  // 게임 폴더 설정
+  const setGamePathCommand = vscode.commands.registerCommand(
+    "rgss-script-compiler.setGamePath",
+    () => {
+      setGamePath(configService, loggingService);
     }
+  );
 
-    const workspaces = vscode.workspace.workspaceFolders;
-    configService.setVSCodeWorkSpace(workspaces[0].uri);
+  const unpackCommand = vscode.commands.registerCommand(
+    "rgss-script-compiler.unpack",
+    () => {
+      if (!configService) {
+        loggingService.info("작업 폴더가 없습니다.");
+        return;
+      }
 
-    loggingService.info("RGSS Script Compiler가 실행되었습니다");
+      loggingService.info(
+        `작업 폴더는 ${configService.getMainGameFolder().fsPath} 입니다.`
+      );
 
-    // 게임 폴더 설정
-    const setGamePathCommand = vscode.commands.registerCommand(
-        "rgss-script-compiler.setGamePath",
-        () => {
-            setGamePath(configService, loggingService);
-        }
-    );
+      const unpacker = new Unpacker(configService, loggingService);
+      unpacker.unpack();
+    }
+  );
 
-    const unpackCommand = vscode.commands.registerCommand(
-        "rgss-script-compiler.unpack",
-        () => {
-            if (!configService) {
-                loggingService.info("작업 폴더가 없습니다.");
-                return;
-            }
+  const compileCommand = vscode.commands.registerCommand(
+    "rgss-script-compiler.compile",
+    () => {
+      if (!configService) {
+        loggingService.info("작업 폴더가 없습니다.");
+        return;
+      }
 
-            loggingService.info(
-                `작업 폴더는 ${configService.getMainGameFolder().path} 입니다.`
-            );
+      loggingService.info(
+        `작업 폴더는 ${configService.getMainGameFolder().fsPath} 입니다.`
+      );
 
-            const unpacker = new Unpacker(configService, loggingService);
-            unpacker.unpack();
-        }
-    );
+      const bundler = new Packer(configService, loggingService);
+      bundler.pack();
+    }
+  );
 
-    const compileCommand = vscode.commands.registerCommand(
-        "rgss-script-compiler.compile",
-        () => {
-            if (!configService) {
-                loggingService.info("작업 폴더가 없습니다.");
-                return;
-            }
+  loggingService.show();
 
-            loggingService.info(
-                `작업 폴더는 ${configService.getMainGameFolder().path} 입니다.`
-            );
-
-            const bundler = new Packer(configService, loggingService);
-            bundler.pack();
-        }
-    );
-
-    loggingService.show();
-
-    context.subscriptions.push(
-        setGamePathCommand,
-        unpackCommand,
-        compileCommand
-    );
+  context.subscriptions.push(setGamePathCommand, unpackCommand, compileCommand);
 }
 
 export function deactivate() {}
