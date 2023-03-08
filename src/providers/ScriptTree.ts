396ea8e3ab9122ad8649083edcb1e260fc94ae69
biud436 <biud436@gmail.com>
2023-03-08 18:21:59 +0900

    docs: added description

--- src/providers/ScriptTree.ts
+++ src/providers/ScriptTree.ts
@@ -1,5 +1,20 @@
 type FilterPredicate<T> = (value: T, index: number, array: T[]) => boolean;
 
+/**
+ * 스크립트를 트리 구조로 관리하기 위한 프록시 클래스이다.
+ * Proxy 클래스로 구현된 이유는, File Watcher와 통합하여 코드의 간결성을 유지하기 위함이다.
+ *
+ * File Watcher는 파일의 변경을 감지하고, 변경된 파일을 트리 구조에 반영한다.
+ * File Watcher가 현재 바깥에 있는데, 이 부분을 나중에 구현하여 통합할 예정이다.
+ *
+ * 하지만 스크립트 트리의 인스턴스가 유일하다는 가정이 있어야 한다.
+ * 그러나 현재는 splice나 filter에 의해 스크립트 트리의 인스턴스가 여러 개 생성되고 있다.
+ * 이 상태에서는 dispose가 여러번 호출되므로 자원을 낭비하게 된다.
+ *
+ * 따라서 아직까진 File Watcher를 통합하기가 힘들다.
+ *
+ * @class ScriptTree
+ */
 export class ScriptTree<T> {
     data: T[];
 
