ee31fc9d4e0c8b305715a094c46f2ab49d68b2d5
biud436 <biud436@gmail.com>
2023-06-03 20:45:12 +0900

    fix: fixed the property called 'Show Status Bar'

--- src/common/WorkspaceValue.ts
+++ src/common/WorkspaceValue.ts
@@ -1,5 +1,12 @@
 export enum WorkspaceValue {
     showStatusBar = "rgssScriptCompiler.showStatusBar",
+    /**
+     * MKXP-Z
+     */
     macOsGamePath = "rgssScriptCompiler.macOsGamePath",
+
+    /**
+     * MKXP-Z
+     */
     macOsBundleIdentifier = "rgssScriptCompiler.macOsBundleIdentifier",
 }
