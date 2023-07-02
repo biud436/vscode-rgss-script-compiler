bae56b6da013dd7327b969daf24deb621edbd98a
biud436 <biud436@gmail.com>
2023-07-02 21:21:28 +0900

    docs: added a new comment

--- src/commands/CheckMigrationNeeded.ts
+++ src/commands/CheckMigrationNeeded.ts
@@ -1,3 +1,8 @@
+/**
+ * Check the extension version (0.0.16 -> 0.1.0)
+ * @param lines
+ * @returns
+ */
 export function checkMigrationNeeded(lines: string[]): boolean {
     const isLineStartsWithNumber = lines.every((line) => {
         return line.match(/^[\d]{3}[\.]*[\d]*\-/);
